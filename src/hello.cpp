3b83886a5cc83b94a3ca698378ee6b6f0789934f
THF <The_Heart_Fickle@163.com>
2023-03-02 22:10:49 +0800

    First Commit

---
+++ src/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "..\include\hello.h"
+
+int Hello()
+{
+    std::cout << "hello,world\n";
+    return 0;
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "..\include\hello.h"
+
+int main(int, char**)
+{
+    Hello();
+    system("pause");
+    return 0;
+}
